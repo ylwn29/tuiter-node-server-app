cac6f4ca17ff3275305d00f71bf6a28a8d008f6d
ylwn29 <ylwn29@gmail.com>
2022-11-29 12:07:19 -0800

    add three tuits files

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    topic: String,
+    username: String,
+    handle: String,
+    time: {
+        type: String,
+        default: "Just now"
+    },
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    title: String,
+    replies: Number,
+    retuits: Number,
+    dislikes: Number,
+    disliked: Boolean,
+}, {collection: 'tuits'});
+export default schema;
+
+//push to github
